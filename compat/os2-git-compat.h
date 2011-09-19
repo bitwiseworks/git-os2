5314107a9af04e45c8aeae821e7cf4d4bca6dd79
Yuri Dario <yd@os2power.com>
2011-09-19 20:58:18 +0000

    git: restored t.ebisawa header.
    git-svn-id: http://svn.netlabs.org/repos/ports/git/trunk@352 8d6eb4b1-0faa-de11-b929-00e081727c95

--- compat/os2-git-compat.h
+++ compat/os2-git-compat.h
@@ -27,9 +27,11 @@ struct pollfd {
 #define POLLIN 1
 #define POLLHUP 2
 
-#define SHUT_WR 2
+#define SHUT_RD         0               /* shut down the reading side */
+#define SHUT_WR         1               /* shut down the writing side */
+#define SHUT_RDWR       2               /* shut down both sides */
 
-//extern int poll (struct pollfd *, nfds_t, int);
+extern int poll (struct pollfd *, nfds_t, int);
 
 extern int wrapped_execl_for_os2 (const char *, char *, ...);
 extern int wrapped_execlp_for_os2 (const char *, char *, ...);
@@ -38,23 +40,23 @@ extern int wrapped_execvp_for_os2 (const char *, char **);
 
 #ifndef BUILDING_COMPAT_OS2
 # ifdef __EMX__
-//#  define chdir(d) _chdir2(d)
-//#  define getcwd(d,n) _getcwd2(d,n)
+#  define chdir(d) _chdir2(d)
+#  define getcwd(d,n) _getcwd2(d,n)
 # endif
 # define getenv(e) wrapped_getenv_for_os2 (e)
 # define getpwuid(u) wrapped_getpwuid_for_klibc (u)
-//# define unlink(f) wrapped_unlink_for_dosish_system (f)
-//# define execl wrapped_execl_for_os2
-//# define execlp wrapped_execlp_for_os2
-//# define execv wrapped_execv_for_os2
-//# define execvp wrapped_execvp_for_os2
+# define unlink(f) wrapped_unlink_for_dosish_system (f)
+# define execl wrapped_execl_for_os2
+# define execlp wrapped_execlp_for_os2
+# define execv wrapped_execv_for_os2
+# define execvp wrapped_execvp_for_os2
 #endif
 
 
-#ifndef _SOCKLEN_T_DECLARED
+//#ifndef _SOCKLEN_T_DECLARED
 //typedef int socklen_t;
-# define _SOCKLEN_T_DECLARED
-#endif
+//# define _SOCKLEN_T_DECLARED
+//#endif
 
 #define has_dos_drive_prefix(path) (isalpha(*(path)) && (path)[1] == ':')
 #define is_dir_sep(c) ((c) == '/' || (c) == '\\')
@@ -68,7 +70,7 @@ extern int git_os2_main(c,v); \
 int main(int argc, char **argv) \
 { \
   git_os2_main_prepare(&argc,&argv); \
-  return git_os2_main(argc, argv); \
+  return git_os2_main(argc, (const char **)argv); \
 } \
 int git_os2_main(c,v)
 
