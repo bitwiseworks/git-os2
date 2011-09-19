40a65e46bef285f8879e5437c4382f80f2dc5edf
Yuri Dario <yd@os2power.com>
2011-09-19 21:01:45 +0000

    git: os2 patches by sava (t.ebisawa) from git 1.6.x.
    git-svn-id: http://svn.netlabs.org/repos/ports/git/trunk@355 8d6eb4b1-0faa-de11-b929-00e081727c95

--- setup.c
+++ setup.c
@@ -48,7 +48,7 @@ char *prefix_path(const char *prefix, int len, const char *path)
 const char *prefix_filename(const char *pfx, int pfx_len, const char *arg)
 {
 	static char path[PATH_MAX];
-#ifndef WIN32
+#if !(defined(__MINGW32__) || defined(__OS2__))
 	if (!pfx_len || is_absolute_path(arg))
 		return arg;
 	memcpy(path, pfx, pfx_len);
